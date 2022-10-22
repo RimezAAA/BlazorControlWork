07d3870ce51d406fcb06e4ae89f8f09b8b7e108f
Арсений Анохин <artuhi.arsenii@gmail.com>
2022-10-22 14:51:03 +0300

    Добавьте файлы проекта.

---
+++ BlazorControlWork/Data/MongoExamples.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+
+namespace BlazorControlWork.Data
+{
+    public class MongoExamples
+    {
+        public static void AddToDB(User user)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("UsersDataBaseArt");
+            var collection = database.GetCollection<User>("Users");
+            collection.InsertOne(user);
+
+        }
+        public static List<User> FindAll()
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("UsersDataBaseArt");
+            var collection = database.GetCollection<User>("Users");
+            var list = collection.Find(x => true).ToList();
+            return list;
+        }
+
+        public static User Find(string login, string password)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("UsersDataBaseArt");
+            var collection = database.GetCollection<User>("Users");
+            var one = collection.Find(x => x.Login == login && x.Password == password).FirstOrDefault();
+            return one;
+        }
+    }
+}
---
+++ BlazorControlWork/Data/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace BlazorControlWork.Data
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId _id;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Login { get; set; }
+
+        public User(string name, string surname, string email, string password, string login)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            Login = login;
+        }
+    }
+}
